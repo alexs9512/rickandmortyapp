27be8f6c3698bff4700b25c260239d1fadff5431
Alexis Jose Palma Ortiz <alexis.palma@globant.com>
2026-06-19 12:07:29 -0600

    Initial Commit

---
+++ Ricky&Morty/Ricky_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ricky_MortyApp.swift
+//  Ricky&Morty
+//
+//  Created by Alexis Jose Palma Ortiz on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Ricky_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
