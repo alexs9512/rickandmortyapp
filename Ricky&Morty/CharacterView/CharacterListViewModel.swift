081f8b3dcb914f91c68631e0a5cd159623280209
Alexis Jose Palma Ortiz <alexis.palma@globant.com>
2026-06-24 16:00:49 -0600

    especicacion de tipo de datos, en characterListViewModel

--- Ricky&Morty/CharacterView/CharacterListViewModel.swift
+++ Ricky&Morty/CharacterView/CharacterListViewModel.swift
@@ -15,6 +15,8 @@ class CharacterListViewModel: ObservableObject {
     
     // MARK: - Published Properties
     @Published var characters: [Character] = []
+  var name: String = ""
+  var age: Int = 18
     
     // MARK: - Init
     init() {
@@ -23,7 +25,7 @@ class CharacterListViewModel: ObservableObject {
     
     // MARK: - API Networking
     func fetchCharacters() {
-        guard let url = URL(string: "https://rickandmortyapi.com/api/character") else { // hacerla explicita
+      guard let url: URL = URL(string: "https://rickandmortyapi.com/api/character") else { // hacerla explicita
             print("URL inválida") // quitar print
             return
         }
@@ -32,10 +34,10 @@ class CharacterListViewModel: ObservableObject {
         Task {
             do {
                 // 1. Descargamos los datos de forma asíncrona
-                let (data, _) = try await URLSession.shared.data(from: url) // variable para el tipo de data
+              let (data, _): (Data, URLResponse) = try await URLSession.shared.data(from: url) // variable para el tipo de data
                 
                 // 2. Decodificamos el JSON de forma segura
-                let decoder = JSONDecoder() // hacer mas explicita 
+                let decoder: JSONDecoder = JSONDecoder() // hacer mas explicita 
                 let decodedResponse = try decoder.decode(CharactersResponse.self, from: data)
                 
                 self.characters = decodedResponse.results
